c8ffddde4b4c312f614300ebc8cc5f78c9690a8c
WeiLin <nomorecomputer@gmail.com>
2025-10-24 08:26:36 +0800

    optimize plotting

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,22 +22,25 @@ const constants = {
   modalOverlayId: "success-modal-overlay",
 };
 
-let tickets = [];
-let modalTimerId;
-let afterModal = null;
+let tickets = []; // 套票資料陣列
+let modalTimerId; // modal 計時器 ID
+let afterModal = null; // modal 關閉後執行的 callback function
 
 //index.htl documentContentLoaded 後執行
 export function InitHandler() {
   registryAddCard(); // 註冊新增套票按鈕
-  registryModalMsg();
+  registryModalMsg(); // 註冊 modal 訊息視窗
   registryChangeAreaOption(); // 註冊 與 處理 地區篩選下拉選單
-  loadTicketJson_InitCards(constants.jsonUrl);
+  loadTicketJson_InitCards(constants.jsonUrl); // 載入 JSON 套票資料，並初始化套票卡片
 }
 
+// 註冊 與 處理 地區篩選下拉選單
 function registryChangeAreaOption() {
   const selectInAddElem = document.getElementById(constants.areaSelectInAddId);
   const selectElem = document.getElementById(constants.areaSelectId);
-  selectElem.addEventListener("change", (e) => initCards(e.target.value));
+  selectElem.addEventListener("change", (e) =>
+    initCards(e.target.value, false)
+  );
   let innerHtml = UI_Template.AreaDefaultOptionHtml;
   let innerHtmlInAdd = UI_Template.AreaDefaultOptionHtml;
   areas.forEach((area, index) => {
@@ -48,7 +51,8 @@ function registryChangeAreaOption() {
   selectInAddElem.innerHTML = innerHtmlInAdd;
   selectElem.innerHTML = innerHtml;
 }
-function initCards(filtBy) {
+// 初始化 套票卡片 filtered by 地區(constants.selectAllValue: 全部),replotting: 是否重繪 圖表
+function initCards(filtBy, replotting = true) {
   const containerElem = document.getElementById(constants.cardContainerId);
   const resultMsgElem = document.getElementById(constants.filterResultId);
   let innerHtml = "";
@@ -60,11 +64,11 @@ function initCards(filtBy) {
     }
   });
   containerElem.innerHTML = innerHtml;
-  areaRatioPlotting();
+  if (replotting) areaRatioPlotting();
   resultMsgElem.innerText = `本次搜尋共 ${resultCount} 筆資料`;
   handleFilterNotFound(resultCount);
 }
-
+// 註冊 新增套票 按鈕 與 表單驗證
 function registryAddCard() {
   const addBtnElem = document.getElementById(
     constants.validator_Constants.addNewCardId
@@ -72,7 +76,7 @@ function registryAddCard() {
   addBtnElem.addEventListener("click", addNewTicket);
   registerFrontValidate(constants.validator_Constants);
 }
-
+// 新增套票
 function addNewTicket() {
   const addTicketFormElem = document.getElementById(constants.newTicketFormId);
   if (!addTicketFormElem.reportValidity()) return;
@@ -92,6 +96,7 @@ function addNewTicket() {
   const modal_overlay_Elem = document.getElementById(
     constants.addCardSucessMsgId
   );
+  // 清除表單內容 與 驗證訊息
   afterModal = () => {
     addTicketFormElem.reset();
     addTicketFormElem
@@ -106,8 +111,8 @@ function addNewTicket() {
   document.getElementById(constants.areaSelectId).value =
     constants.selectAllValue;
 }
-
-function loadTicketJson_InitCards(url, responseTimeout = 5000) {
+// 載入 JSON 套票資料，並初始化套票卡片
+function loadTicketJson_InitCards(url, responseTimeout = 6000) {
   axios
     .get(url, { timeout: responseTimeout })
     .then(function (response) {
@@ -121,17 +126,17 @@ function loadTicketJson_InitCards(url, responseTimeout = 5000) {
       let errMsg = error.message;
       if (error.stack) errMsg += error.stack;
       if (error.response) errMsg += error.response;
-      if (error.request) errMst += error.request;
+      if (error.request) errMsg += error.request;
 
       alert(`載入 JSON失敗；請檢察網路連線、或URL ${url}是否正確`);
     });
 }
-
+// 處理 篩選無結果 顯示訊息
 function handleFilterNotFound(resultCount) {
   const msgArea = document.getElementById(constants.canotFindAreaId);
   msgArea.style.display = resultCount == 0 ? "block" : "none";
 }
-
+// 註冊 modal 訊息視窗 點擊關閉事件 ， 如果有設定 afterModal 會在關閉後執行
 function registryModalMsg() {
   const modalElem = document.querySelector(
     `#${constants.modalOverlayId} .modal`
@@ -142,6 +147,7 @@ function registryModalMsg() {
     modalElem.parentElement.classList.remove("active");
   });
 }
+// 顯示 modal 訊息視窗 timeout: 自動關閉時間(毫秒) aftershowHandler: 自動關閉後執行的 callback function
 function modal_msg_show(msg, timeout = 3000, aftershowHandler = null) {
   const modalOverlayElem = document.getElementById(constants.modalOverlayId);
   modalOverlayElem.querySelector(".alt-msg-text").textContent = msg;
@@ -151,30 +157,24 @@ function modal_msg_show(msg, timeout = 3000, aftershowHandler = null) {
     if (aftershowHandler != null) aftershowHandler();
   }, timeout);
 }
-
+// 關閉 modal 訊息視窗 afterCloseHandler: 關閉後執行的 callback function
 function modal_msg_close(afterCloseHandler = null) {
   const modalOverlayElem = document.getElementById(constants.modalOverlayId);
   modalOverlayElem.classList.remove("active");
 }
-
+// 套票地區比重 圖表繪製
 function areaRatioPlotting() {
   const areaCountArr = {};
-  tickets.forEach((ticket) => {
-    if (areaCountArr[ticket.area] == undefined) {
-      areaCountArr[ticket.area] = 1;
-    } else {
-      areaCountArr[ticket.area] += 1;
-    }
-  });
-  const plotData = [];
-  for (const area in areaCountArr) {
-    plotData.push([area, areaCountArr[area]]);
-  }
+  tickets.forEach(
+    (ticket) =>
+      (areaCountArr[ticket.area] = (areaCountArr[ticket.area] || 0) + 1)
+  );
+
   const chart = c3.generate({
     bindto: "#areaRatioPlotting",
     size: { width: 162, height: 192 },
     data: {
-      columns: plotData,
+      columns: Object.entries(areaCountArr),
       type: "donut",
     },
     donut: {
